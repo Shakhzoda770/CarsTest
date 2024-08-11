9e45f721062089b7f4f3e615bb26e3290497940d
shakshzoda <shakha1526@gmail.com>
2024-08-11 10:55:19 -0600

    changes

--- config/config.py
+++ config/config.py
@@ -1,4 +1,3 @@
 # config.py
 BROWSER_TYPE = "chromium"  # Options: "chromium", "firefox", "webkit"
 HEADLESS_MODE = False       # Set to True for headless mode, False for headed mode
-BROWSER_URL = "https://www.google.co.in/"
